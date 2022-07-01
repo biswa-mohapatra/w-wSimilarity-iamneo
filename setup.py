21de41efb0ebcd41ed5b3e2c531246bc3d45a15a
BISWAJIT RAJAGURU MOHAPATRA <68077268+biswa-mohapatra@users.noreply.github.com>
2022-07-01 12:05:12 +0530

    template update

--- setup.py
+++ setup.py
@@ -4,21 +4,21 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 ## edit below variables as per your requirements -
-REPO_NAME = "MLflow-project-template"
-AUTHOR_USER_NAME = "c17hawke"
+REPO_NAME = "w-wSimilarity-iamneo"
+AUTHOR_USER_NAME = "biswa-mohapatra"
 SRC_REPO = "src"
 LIST_OF_REQUIREMENTS = []
 
 
 setup(
     name=SRC_REPO,
-    version="0.0.1",
+    version="1.0.0",
     author=AUTHOR_USER_NAME,
-    description="A small package for MLflow app",
+    description="A small package for finding word-word similarity.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
-    author_email="sunny.c17hawke@gmail.com",
+    author_email="biswajit@iamneo.ai",
     packages=[SRC_REPO],
     license="MIT",
     python_requires=">=3.6",
